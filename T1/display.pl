1b5c86af650f30fdf638c7bf4b6ddda04e80a693
Andreia Gouveia <andiigouveia@hotmail.com>
2019-10-13 00:02:07 +0100

    added variable to pieces

--- T1/display.pl
+++ T1/display.pl
@@ -1,7 +1,19 @@
 initialBoard(
-                [['*','*','*','*'],['*','*','*','*'],['*','*','*','*'],['*','*','*','*']]).
+                [[empty,empty,empty,empty],[empty,empty,empty,empty],[empty,empty,empty,empty],[empty,empty,empty,empty]]).
 
 printBoard(Board):-write('  ----------------') , printLines(Board).
+
+piece(empty, V) :- V = '*'.
+piece(coneB, V) :- V ='p'.
+piece(coneW, V) :- V ='P'.
+piece(cubeB, V) :- V ='c'.
+piece(cubeW, V) :- V ='C'.
+piece(cylinderB, V) :- V ='l'.
+piece(cylinderW, V) :- V ='L'.
+piece(sphereB, V) :- V ='e'.
+piece(sphereW, V) :- V ='E'.
+
+
 
 printLines([]):-
         nl.
@@ -18,5 +30,6 @@ printColumn([]):-
 
 printColumn([H|T]):-
         write(' | '),
-        write(H),
+        piece(H,Piece),
+        write(Piece),
         printColumn(T).
