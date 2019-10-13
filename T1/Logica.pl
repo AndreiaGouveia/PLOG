93693ae865a9c1bde560ea54229b2d0f497d1a15
jfcaraujo <jfcaraujo99@gmail.com>
2019-10-13 16:19:05 +0100

    Fixed getCoord

--- T1/Logica.pl
+++ T1/Logica.pl
@@ -15,20 +15,22 @@ getY(Y):-
 getCoord(X,Y):-
 	getX(X),
 	getY(Y),
-	initialBoard(Board),
+	initialBoard(Board),%must find way to use global board
 	validMove(X,Y,Board).
 
 validMove(0,0,H):-
-	H==empty.
+	H=='empty'.
 
-validMove(1,0,[H|T]):-
+validMove(1,0,[H|_]):-
 	validMove(0,0,H).
 
-validMove(X,0,[H|T]):-
-	validMove(X-1,0,T).
+validMove(X,0,[_|T]):-
+	X1 is X-1,
+	validMove(X1,0,T).
 
-validMove(X,1,[H|T]):-
+validMove(X,1,[H|_]):-
 	validMove(X,0,H).
 
-validMove(X,Y,[H|T]):-
-	validMove(X,Y-1,T).
+validMove(X,Y,[_|T]):-
+	Y1 is Y-1,
+	validMove(X,Y1,T).
