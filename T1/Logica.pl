e18c27f9e51df7c7128764d8f443ce9311a7f81a
jfcaraujo <jfcaraujo99@gmail.com>
2019-10-13 13:38:11 +0100

    Completed getCoord

--- T1/Logica.pl
+++ T1/Logica.pl
@@ -1,2 +1,17 @@
-localvalido(X,Y):-
-        X > -1,Y > -1,X < 4,Y < 4 ->true.
+getX(X):-
+	repeat,
+	write('O valor de X tem de ser entre 1 e 4\n'),
+	read(X),
+	X=<4,
+	X>=1.
+
+getY(Y):-
+	repeat,
+	write('O valor de Y tem de ser entre 1 e 4\n'),
+	read(Y),
+	Y=<4,
+	Y>=1.
+
+getCoord(X,Y):-
+	getX(X),
+	getY(Y).
