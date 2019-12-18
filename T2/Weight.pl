44dcf85b46376f0240a7ef5332556fcc79194ee8
jfcaraujo <jfcaraujo99@gmail.com>
2019-12-18 11:39:24 +0000

    Added more tests

--- T2/Weight.pl
+++ T2/Weight.pl
@@ -3,11 +3,13 @@
 :- use_module(library(clpfd)).
 
 %for sublist, first element is position, second element is number of weights
-dist([-3,-1,[2,3,-2,-1,1]]).%5 elements
-dist2([-3,-2,[1,3,[-1,2,-3,1],1],2]).%6 elements
-dist3([[-1,5,[-1,4,[-1,3,-1,1,2],3],2],[1,3,-2,1,2],[2,2,-2,3]]).%10 elements
+dist(5,[-3,-1,[2,3,-2,-1,1]]).%5 elements
+dist(6,[-3,-2,[1,3,[-1,2,-3,1],1],2]).%6 elements
+dist(10,[[-1,5,[-1,4,[-1,3,-1,1,2],3],2],[1,3,-2,1,2],[2,2,-2,3]]).%10 elements
+dist(14,[[-2,3,-2,-1,5],[1,11,[-1,9,[-1,7,-2,-1,[1,5,[-1,3,-2,[1,2,-1,2]],1,2]],1,2],1,3]]).%14
+dist(20,[[-4,3,-3,-2,3],[-2,7,-2,-1,[1,5,[-1,3,-1,2,3],1,2]],1,[3,9,[-1,7,[-1,5,-1,[1,2,-2,1],2,3],1,2],1,2]]).%20
 
-
+test(X,R):-dist(X,List),game(X,List,R). % X=5,6,10,14 or 20
 game(Size,Distances,Weights):-
 	length(Weights, Size),
     domain(Weights,1,Size),
@@ -30,8 +32,12 @@ sumWeights([H1|Distances],[H2|Weights],Acumulator):-%if H1 is not list
 	sumWeights(Distances,Weights,AuxAcumulator),
 	Acumulator #= H1 * H2 + AuxAcumulator.
 
-first_n(0,_,[]).
-first_n(N, [H|T], [H|T1]) :- N > 0, N1 is N-1, first_n(N1, T, T1).
+%first_n(0,_,[]).
+%first_n(N, [H|T], [H|T1]) :- N > 0, N1 is N-1, first_n(N1, T, T1).
+first_n(N, List, AuxList) :-
+	length(AuxList,N),
+	append(AuxList, _, List).
+	
 
 multiply(_,[],0).
 
