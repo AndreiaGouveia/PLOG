e3ae8de0c4e327de55f811a7def02c1ac7f032e9
jfcaraujo <jfcaraujo99@gmail.com>
2020-01-04 18:32:28 +0000

    Final commit?

--- T2/Weight.pl
+++ T2/Weight.pl
@@ -12,7 +12,8 @@ dist(19,[[-2,8,-3,-1,[1,6,[-1,4,-2,-1,[1,2,-1,2]],1,2]],1,[2,10,-3,-1,[1,8,[-1,5
 dist(20,[[-4,3,-3,-2,3],[-2,7,-2,-1,[1,5,[-1,3,-1,2,3],1,2]],1,[3,9,[-1,7,[-1,5,-1,[1,2,-2,1],2,3],1,2],1,2]]).%20
 
 
-weight(X):-dist(X,List),game(X,List,R),display(List,R,[],[]). % X=5,6,10,14,19 or 20
+weight(X):-dist(X,List),game(X,List,R),
+write('Distances: '),write(List),nl,write('Weights: '),write(R),nl,display(List,R,[],[]). % X=5,6,10,14,19 or 20
 game(Size,Distances,Weights):-
 	length(Weights, Size),
     domain(Weights,1,Size),
