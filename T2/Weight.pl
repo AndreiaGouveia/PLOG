512470a2ed6a451251bb152b3b62fe9ab0b64747
jfcaraujo <jfcaraujo99@gmail.com>
2020-01-05 23:17:32 +0000

    Added note

--- T2/Weight.pl
+++ T2/Weight.pl
@@ -16,7 +16,7 @@ dist(20,[[-4,3,-3,-2,3],[-2,7,-2,-1,[1,5,[-1,3,-1,2,3],1,2]],1,[3,9,[-1,7,[-1,5,
 
 
 weight(X):-dist(X,List),game(X,List,R),
-write('Distances: '),write(List),nl,write('Weights: '),write(R),nl,display(List,R,[],[]). % X=5,6,10,14,19 or 20
+write('Distances: '),write(List),nl,write('Weights: '),write(R),nl,display(List,R,[],[]). % X=5,6,7,8,9,10,14,19 or 20
 game(Size,Distances,Weights):-
 	%Variaveis de decisao
 	length(Weights, Size),
